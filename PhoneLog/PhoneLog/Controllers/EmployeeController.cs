2a8f5028a9e2189ebba97350f6bbc505f134bfe3
Richard <richard.garbi@gmail.com>
2013-03-23 13:39:54 -0500

    Adding more controllers.

---
+++ PhoneLog/PhoneLog/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Linq;
+
+namespace PhoneLog.Controllers
+{
+    public class EmployeeController
+    {
+
+        public static void storeEmployee(string employeeName, String employeeEmail)
+        {
+            using (var db = new PhoneLog.Models.PhoneLogContext())
+            {
+                var employee = new PhoneLog.Models.Employee
+                {
+                    EmployeeName = employeeName,
+                    EmployeeEmail = employeeEmail
+                    
+                };
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+        }
+
+        public static List<PhoneLog.Models.Employee> getAllEmployees()
+        {
+            List<Models.Employee> allEmployees = new List<Models.Employee>();
+
+            using (var db = new PhoneLog.Models.PhoneLogContext())
+            {
+                var query = from l in db.Employees
+                            orderby l.EmployeeName
+                            select l;
+
+                Console.WriteLine("All employees in the database:");
+                foreach (var item in query)
+                {
+                    allEmployees.Add(new Models.Employee
+                    {
+                        Id = item.Id,
+                        EmployeeName = item.EmployeeName,
+                        EmployeeEmail = item.EmployeeEmail
+                    });
+                }
+            }
+
+            return allEmployees;
+        }
+
+        public static void deleteEmployee(int id)
+        {
+            using (var db = new PhoneLog.Models.PhoneLogContext())
+            { 
+                var employee = db.Employees.Where(item => item.Id == id).Single();
+                db.Employees.Remove(employee);
+                db.SaveChanges();
+            }
+        }
+
+        public static Models.Employee getEmployeeById(int id)
+        {
+            using (var db = new PhoneLog.Models.PhoneLogContext())
+            {
+                var employee = db.Employees.Where(item => item.Id == id).Single();
+                return new Models.Employee
+                {
+                    Id = employee.Id,
+                    EmployeeName = employee.EmployeeName,
+                    EmployeeEmail = employee.EmployeeEmail
+                };
+            }
+        }
+    }
+}
---
+++ PhoneLog/PhoneLogTest/EmployeeControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneLog.Controllers;
+using System.Collections.Generic;
+using PhoneLog.Models;
+
+namespace PhoneLogTest
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        
+
+        [TestMethod]
+        public void storeLog()
+        {
+            EmployeeController.storeEmployee("EmployeeName", "EmployeeEmail");
+
+            List<Employee> allEmployees = EmployeeController.getAllEmployees();
+
+            foreach(Employee e in allEmployees)
+            {
+                Console.WriteLine(e.Id);
+                Console.WriteLine(e.EmployeeName);
+                Console.WriteLine(e.EmployeeEmail);
+
+                EmployeeController.deleteEmployee(e.Id);
+            }
+
+        }
+    }
+}
+    
