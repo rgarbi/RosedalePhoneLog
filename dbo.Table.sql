c49a4d638559c36df6cd9f2e97c84ea5feb1201c
Richard <richard.garbi@gmail.com>
2013-03-23 00:30:27 -0500

    Adding phone log entity and database

---
+++ dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CallerName] VARCHAR(MAX) NULL, 
+    [PhoneNumber] NVARCHAR(50) NULL, 
+    [Date] DATETIME NULL, 
+    [Message] VARCHAR(MAX) NULL, 
+    [Employee] VARCHAR(50) NULL, 
+    [CallType] NCHAR(10) NULL, 
+    [FollowUp] BIT NULL
+)
